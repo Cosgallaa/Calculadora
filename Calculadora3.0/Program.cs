bfd2a0aa4fdd53e5e67e4e83c3166ee7b2e1a6c4
Cosgallaa <acjosecgrey@gmail.com>
2023-06-14 09:47:56 -0500

    Agregar archivos de proyecto.

---
+++ Calculadora3.0/Program.cs
@@ -0,0 +1,15 @@
+﻿using Calculadora3._0.ENTITIES;
+
+namespace Calculadora3._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+                 Usuario usuario = new Usuario();
+                usuario.imprmirPeticion();
+        }
+        
+    }
+}
+    
